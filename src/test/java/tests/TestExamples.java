cd5ff606d7b0f6115ade6de456eddd19a212e145
Yuri Kulagin <kulagin1987@gmail.com>
2022-06-21 00:12:09 +0300

    initial commit

--- src/test/java/tests/TestExamples.java
+++ src/test/java/tests/TestExamples.java
@@ -1,5 +1,6 @@
 package tests;
 
+import com.beust.jcommander.Parameter;
 import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.logevents.SelenideLogger;
 import io.qameta.allure.*;
@@ -18,89 +19,51 @@ public class TestExamples {
 
     @Test
     @Owner("kulaginyv")
-    @Severity(SeverityLevel.CRITICAL)
-    @Feature("Список врачей")
-    @Story("Можно найти страницу врача через поиск")
-    @DisplayName("Тестируем поиск врача")
-    @Description("Функциональный тест")
-    @Link(name = "webprizma", url = "https://webprizma.ru")
-    void firstTest() {
-        SelenideLogger.addListener("allure", new AllureSelenide());
-
-        step("Открываем сайт docdoc.ru", () -> {
-            open("https://docdoc.ru/");
-        });
-        step("Вводим специальность " + speciality, () -> {
-            $("div.search-form__input--specialities input").sendKeys(speciality);
-            $$("div[data-test-id='search_items'] span").findBy(Condition.text(speciality)).click();
-        });
-        step("Вводим метро " + metro, () -> {
-            $("div.search-form__input--geo input").sendKeys(metro);
-            $$("div[data-test-id='search_geo_items'] span").findBy(Condition.text(metro)).click();
-        });
-        step("Нажимаем кнопку Искать", () -> {
-            $("button.search-form__button").click();
-        });
-        step("Открываем страницу врача " + doctor, () -> {
-            $$("a[data-test-id='doctor-list-page-card-details__link']")
-                    .findBy(Condition.text(doctor))
-                    .click();
-        });
-        step("Проверяем, что открылась верная страница врача", () -> {
-            $("h1[data-testid='doctor-card__name']")
-                    .shouldHave(Condition.text(doctor));
-        });
-    }
-
-    @Test
-    @Owner("kulaginyv")
-    @Severity(SeverityLevel.NORMAL)
-    @Feature("Список врачей")
-    @Story("Можно найти страницу врача через поиск только по специальности")
-    @DisplayName("Тестируем поиск врача только по специальности")
-    @Description("Функциональный тест")
-    @Link(name = "webprizma", url = "https://webprizma.ru")
-    void secondTest() {
-        SelenideLogger.addListener("allure", new AllureSelenide());
-
-        step("Открываем сайт docdoc.ru", () -> {
-            open("https://docdoc.ru/");
-        });
-        step("Вводим специальность " + speciality, () -> {
-            $("div.search-form__input--specialities input").sendKeys(speciality);
-            $$("div[data-test-id='search_items'] span").findBy(Condition.text(speciality)).click();
-        });
-        step("Нажимаем кнопку Искать", () -> {
-            $("button.search-form__button").click();
-        });
-        step("Открываем страницу врача " + doctor, () -> {
-            $$("a[data-test-id='doctor-list-page-card-details__link']")
-                    .findBy(Condition.text(doctor))
-                    .click();
-        });
-        step("Проверяем, что открылась верная страница врача", () -> {
-            $("h1[data-testid='doctor-card__name']")
-                    .shouldHave(Condition.text(doctor));
-        });
-    }
-
-    @Test
-    @Owner("kulaginyv")
     @Severity(SeverityLevel.BLOCKER)
     @Feature("Список врачей")
-    @Story("Можно найти страницу врача через поиск только по метро")
-    @DisplayName("Тестируем поиск врача только по метро")
+    @Story("Можно найти страницу врача через поиск")
+    @DisplayName("Чистый Selenide (с Listener)")
     @Description("Функциональный тест")
-    @Link(name = "webprizma", url = "https://webprizma.ru")
-    void thirdTest() {
+    @Link(name = "docdoc.ru", url = "https://docdoc.ru/")
+    void firstTest() {
         SelenideLogger.addListener("allure", new AllureSelenide());
+        Allure.parameter("Доктор", "Балканова Вероника Сергеевна");
+        Allure.parameter("Метро", "Авиамоторная");
+        Allure.parameter("Специальность", "Аритмолог");
+
+        open("https://docdoc.ru/");
+        $("div.search-form__input--specialities input").sendKeys(speciality);
+        $$("div[data-test-id='search_items'] span").findBy(Condition.text(speciality)).click();
+        $("div.search-form__input--geo input").sendKeys(metro);
+        $$("div[data-test-id='search_geo_items'] span").findBy(Condition.text(metro)).click();
+        $("button.search-form__button").click();
+        $$("a[data-test-id='doctor-list-page-card-details__link']")
+                    .findBy(Condition.text(doctor))
+                    .click();
+        $("h1[data-testid='doctor-card__name']")
+                    .shouldHave(Condition.text(doctor));
+    }
+
+    @Test
+    @Owner("kulaginyv")
+    @Severity(SeverityLevel.MINOR)
+    @Feature("Список врачей")
+    @Story("Можно найти страницу врача через поиск только по специальности")
+    @DisplayName("Лямбда шаги через step (name, () -> {})")
+    @Description("Функциональный тест")
+    @Link(name = "docdoc.ru", url = "https://docdoc.ru/")
+    void secondTest() {
+        SelenideLogger.addListener("allure", new AllureSelenide());
+        Allure.parameter("Доктор", "Балканова Вероника Сергеевна");
+        Allure.parameter("Метро", "Авиамоторная");
+        Allure.parameter("Специальность", "Аритмолог");
 
         step("Открываем сайт docdoc.ru", () -> {
             open("https://docdoc.ru/");
         });
-        step("Вводим метро " + metro, () -> {
-            $("div.search-form__input--geo input").sendKeys(metro);
-            $$("div[data-test-id='search_geo_items'] span").findBy(Condition.text(metro)).click();
+        step("Вводим специальность " + speciality, () -> {
+            $("div.search-form__input--specialities input").sendKeys(speciality);
+            $$("div[data-test-id='search_items'] span").findBy(Condition.text(speciality)).click();
         });
         step("Нажимаем кнопку Искать", () -> {
             $("button.search-form__button").click();
@@ -114,12 +77,34 @@ public class TestExamples {
             $("h1[data-testid='doctor-card__name']")
                     .shouldHave(Condition.text(doctor));
         });
+    }
+
+    @Test
+    @Owner("kulaginyv")
+    @Severity(SeverityLevel.TRIVIAL)
+    @Feature("Список врачей")
+    @Story("Можно найти страницу врача через поиск только по метро")
+    @DisplayName("Шаги с аннотацией @Step")
+    @Description("Функциональный тест")
+    @Link(name = "docdoc.ru", url = "https://docdoc.ru/")
+    void thirdTest() {
+        SelenideLogger.addListener("allure", new AllureSelenide());
+        Allure.parameter("Доктор", "Балканова Вероника Сергеевна");
+        Allure.parameter("Метро", "Авиамоторная");
+        Allure.parameter("Специальность", "Аритмолог");
+        WebSteps ws = new WebSteps();
+
+        ws.openMainPage();
+        ws.selectMetro(metro);
+        ws.searchSubmit();
+        ws.openDoctorPage(doctor);
+        ws.checkDoctorPage(doctor);
     }
 
     @Test
     public void dynamicLabels() {
         Allure.label("owner", "yuri_kulagin");
-        Allure.label("severity", "trivial"); // просто передаем строкой
+        Allure.label("severity", SeverityLevel.CRITICAL.value());
         Allure.feature("Динамические таблички");
         Allure.story("Используем динамические таблички");
         Allure.getLifecycle().updateTestCase(testResult -> testResult.setName("Тест с использованием динамическим табличек"));
---
+++ src/test/java/tests/WebSteps.java
@@ -0,0 +1,34 @@
+package tests;
+
+import com.codeborne.selenide.Condition;
+import io.qameta.allure.Step;
+
+import static com.codeborne.selenide.Selenide.*;
+import static io.qameta.allure.Allure.step;
+
+public class WebSteps {
+     @Step("Открываем сайт docdoc.ru")
+     public void openMainPage() {
+          open("https://docdoc.ru/");
+     }
+     @Step("Вводим метро {metro}")
+     public void selectMetro(String metro) {
+          $("div.search-form__input--geo input").sendKeys(metro);
+          $$("div[data-test-id='search_geo_items'] span").findBy(Condition.text(metro)).click();
+     }
+     @Step("Нажимаем кнопку Искать")
+     public void searchSubmit() {
+          $("button.search-form__button").click();
+     }
+     @Step("Открываем страницу врача {doctor}")
+     public void openDoctorPage(String doctor) {
+          $$("a[data-test-id='doctor-list-page-card-details__link']")
+                  .findBy(Condition.text(doctor))
+                  .click();
+     }
+     @Step("Проверяем, что открылась верная страница врача")
+     public void checkDoctorPage(String doctor) {
+          $("h1[data-testid='doctor-card__name']")
+                  .shouldHave(Condition.text(doctor));
+     }
+}
